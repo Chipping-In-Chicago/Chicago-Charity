889a722a5f4cb5e4e9ff37eaa8ad4aa9fc628a6e
Flunder620 <jguzman12310@yahoo.com>
2024-01-03 19:56:32 -0600

    added Sass for css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,25 +3,17 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App.jsx';
 import Home from './pages/Home';
-import Matchup from './pages/Matchup';
-import Vote from './pages/Vote';
-import NotFound from './pages/NotFound';
+import ErrorPage from './pages/Errorpage.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <NotFound />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
         element: <Home />
-      }, {
-        path: '/matchup',
-        element: <Matchup />
-      }, {
-        path: '/matchup/:id',
-        element: <Vote />
       },
     ],
   },
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import "./css/Home.scss";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Location 1 Current Item List:</h1>
+      <div>
+        <h3> Current number of Medical Items donated:</h3>
+        <ul>
+            <li>
+                Feminine Products
+            </li>
+            <li>
+                Hand Sanitizer
+            </li>
+            <li>
+                Masks:
+            </li>
+        </ul>
+      </div>
+      <div>
+        <h3> Current number of Food Items donated:</h3>
+      </div>
+      <div>
+        <h3> Current number of Clothing Items donated:</h3>
+      </div>
+    </>
+  );
+};
+
+export default Home;
