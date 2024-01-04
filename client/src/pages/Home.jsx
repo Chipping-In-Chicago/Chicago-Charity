43d88fd8ef2f55347efdb8638961251f9bb9750b
Flunder620 <jguzman12310@yahoo.com>
2024-01-03 20:37:35 -0600

    added template to home page:

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,25 +4,32 @@ const Home = () => {
   return (
     <>
       <h1>Location 1 Current Item List:</h1>
-      <div>
-        <h3> Current number of Medical Items donated:</h3>
-        <ul>
-            <li>
-                Feminine Products
-            </li>
-            <li>
-                Hand Sanitizer
-            </li>
-            <li>
-                Masks:
-            </li>
-        </ul>
-      </div>
-      <div>
-        <h3> Current number of Food Items donated:</h3>
-      </div>
-      <div>
-        <h3> Current number of Clothing Items donated:</h3>
+      <div className="categories">
+        <div>
+          <h3> Current number of Medical Items donated:</h3>
+          <ul className="item-list">
+            <li>Feminine Products:</li>
+            <li>Hand Sanitizer:</li>
+            <li>Masks:</li>
+          </ul>
+        </div>
+        <div>
+          <h3> Current number of Food Items donated:</h3>
+          <ul className="item-list">
+            <li>Canned Food:</li>
+            <li>Fruit:</li>
+            <li>Meat:</li>
+          </ul>
+        </div>
+        <div>
+          <h3> Current number of Clothing Items donated:</h3>
+          <ul className="item-list">
+            <li>Socks:</li>
+            <li>Underwear:</li>
+            <li>Hats:</li>
+          </ul>
+        </div>
+        <button className="btn">I need to help</button>
       </div>
     </>
   );
