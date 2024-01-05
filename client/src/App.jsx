40433b56e1fde08e5e992a7cd82c332495419001
Fanny Tan <fannyc310@gmail.com>
2024-01-04 19:22:14 -0600

    add Navbar and footer components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,41 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+// import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
-const client = new ApolloClient({
-  uri: '/graphql',
-  cache: new InMemoryCache(),
-});
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <ApolloProvider client={client}>
+    < >
       <div className="flex-column justify-center align-center min-100-vh bg-primary">
+        <Navbar />
+        <div className='container'>
         <Outlet />
+        </div>
+        <Footer />
       </div>
-    </ApolloProvider>
+    </>
   );
 }
 
 export default App;
+
+// const client = new ApolloClient({
+//   uri: '/graphql',
+//   cache: new InMemoryCache(),
+// });
+
+// function App() {
+//   return (
+//     <ApolloProvider client={client}>
+//       <div className="flex-column justify-center align-center min-100-vh bg-primary">
+//         <Navbar />
+//         <div className='container'>
+//         <Outlet />
+//         </div>
+//         <Footer />
+//       </div>
+//     </ApolloProvider>
+//   );
+// }
