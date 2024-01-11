aecc88a2d7340d381825118c57a256eeabba0343
Scotty Jirik <jirikscotty@gmail.com>
2024-01-10 19:04:59 -0600

    I added scss imports to all the pages, and fleshed out the About us page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,12 @@
+import "./css/About.scss";
+
 export default function About() {
   return (
     <>
       <h1>About Us</h1>
+      <p>As our mission, we would like to inform people of the right products to donate based on the current products that have already been donated. This is to represent the positive impact an individual can have on the city of Chicago</p>
+      <h2>Contact Us</h2>
+      <p>To contact us, email us at chip-in-chicago@gmail.com</p>
     </>
   );
 }
