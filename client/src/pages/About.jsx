ba5032c950c85594ca02fd13ae36d3f54236f26d
Scotty Jirik <jirikscotty@gmail.com>
2024-01-10 19:34:16 -0600

    further updated Home scss, and added scss to the about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,9 +4,10 @@ export default function About() {
   return (
     <>
       <h1>About Us</h1>
-      <p>As our mission, we would like to inform people of the right products to donate based on the current products that have already been donated. This is to represent the positive impact an individual can have on the city of Chicago</p>
+      <p className="text">As our mission, we would like to inform people of the right products to donate based on the current products that have already been donated. This is to represent the positive impact an individual can have on the city of Chicago</p>
       <h2>Contact Us</h2>
-      <p>To contact us, email us at chip-in-chicago@gmail.com</p>
+      <p className="text">To contact us, email us at chip-in-chicago@gmail.com</p>
+      <Link to = '/signup' className="btn">I need to help</Link>
     </>
   );
 }
