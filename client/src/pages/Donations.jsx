21ca965c07bf1041ebee6dc00be93b91aecbdffb
Scotty Jirik <jirikscotty@gmail.com>
2024-01-11 20:37:42 -0600

    I added a 0 option for the home page, then I updated the readme further with a screenshot

--- client/src/pages/Donations.jsx
+++ client/src/pages/Donations.jsx
@@ -9,18 +9,21 @@ export default function Donations() {
           <h2 className="category-name medical">Medical</h2>
           <label for="feminine-products">Feminine Products</label>
           <select name="feminine-products">
+            <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
           </select>
           <label for="hand-sanitizer">Hand Sanitizer</label>
           <select name="hand-sanitizer">
+            <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
           </select>
           <label for="masks">Masks</label>
           <select name="masks">
+            <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -30,18 +33,21 @@ export default function Donations() {
           <h2 className="category-name food">Food</h2>
           <label for="canned-food">Canned Food</label>
           <select name="canned-food">
+            <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
           </select>
           <label for="fruit">Fruit</label>
           <select name="fruit">
+            <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
           </select>
           <label for="meat">Meat</label>
           <select name="meat">
+            <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -51,18 +57,21 @@ export default function Donations() {
           <h2 className="category-name clothing">Clothing</h2>
           <label for="socks">Socks</label>
           <select name="socks">
+            <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
           </select>
           <label for="underwear">Underwear</label>
           <select name="underwear">
+            <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
           </select>
           <label for="hats">Hats</label>
           <select name="hats">
+            <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
