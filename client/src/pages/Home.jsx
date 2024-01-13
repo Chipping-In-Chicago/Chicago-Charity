b95f0adf06e070388e89bf2ed09750532172c95c
Flunder620 <jguzman12310@yahoo.com>
2024-01-12 18:39:40 -0600

    added displaying data functionality

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,37 +7,42 @@ export default function Home() {
   const { loading, data } = useQuery(QUERY_ITEMS)
   console.log('data:', data)
   console.log('loading:', loading)
-  // const itemList = data?.items || [];
+  const item = data?.getItems || [];
+  console.log({item})
+
 
   return (
     <>
-      <div className="categories">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+      <div key={item} className="categories">
         <div>
           <h3> Current number of Medical Items donated:</h3>
           <ul className="item-list">
-            <li>Feminine Products:</li>
-            <li>Hand Sanitizer:</li>
-            <li>Masks:</li>
+            <li>Feminine Products: {item[0].itemCount}</li>
+            <li>Hand Sanitizer: {item[3].itemCount}</li>
+            <li>Masks: {item[1].itemCount}</li>
           </ul>
         </div>
         <div>
           <h3> Current number of Food Items donated:</h3>
           <ul className="item-list">
-            <li>Canned Food:</li>
-            <li>Fruit:</li>
-            <li>Meat:</li>
+            <li>Canned Food: {item[4].itemCount}</li>
+            <li>Fruit: {item[5].itemCount}</li>
+            <li>Meat: {item[7].itemCount}</li>
           </ul>
         </div>
         <div>
           <h3> Current number of Clothing Items donated:</h3>
           <ul className="item-list">
-            <li>Socks:</li>
-            <li>Underwear:</li>
-            <li>Hats:</li>
+            <li>Socks: {item[6].itemCount}</li>
+            <li>Underwear: {item[2].itemCount}</li>
+            <li>Hats: {item[8].itemCount}</li>
           </ul>
         </div>
         <Link to = '/signup' className="btn">I need to help</Link>
-      </div>
+      </div>)}
     </>
   );
 }
