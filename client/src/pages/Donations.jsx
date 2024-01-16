40452c2e7a1cbd416d58592e847668ff36b0ce97
Flunder620 <jguzman12310@yahoo.com>
2024-01-15 19:01:17 -0600

    ability to save values added

--- client/src/pages/Donations.jsx
+++ client/src/pages/Donations.jsx
@@ -1,41 +1,60 @@
 import "./css/Donations.scss";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Donations() {
-const [selectedValue, setSelectedValue] = useState([]);
 
-const handleDropdownChange = (e,index) => {
-  e.preventDefault()
-  const newSelectedValue = [...selectedValue]
-  newSelectedValue[index]= e.target.value;
-  setSelectedValue(newSelectedValue)
-  // setSelectedValue(e.target.value)
-  // console.log(e.target.value)
-}
+const [selectedValues, setSelectedValues] = useState({
+  feminineProducts: '0',
+  handSanitizer: '0',
+  masks: '0',
+  cannedFood: '0',
+  fruit: '0',
+  meat: '0',
+  socks: '0',
+  underwear: '0',
+  hats: '0'
+});
+
+
+useEffect(() => {
+  // Side effect code to handle the selected values
+  console.log(selectedValues);
+}, [selectedValues]);
+
+const handleDropdownChange = (e, dropdownName) => {
+  // const { name, value } = e.target;
+  setSelectedValues((prevValues) => ({
+    ...prevValues,
+    [dropdownName]: e.target.value,
+  }));
+};
+
+
+
 
   return (
     <>
       <h1 className="question">What would you like to donate today?</h1>
-      <form onSubmit={handleDropdownChange}>
+      <form>
       <div className="donation-categories">
         <div>
           <h2 className="category-name medical">Medical</h2>
           <label htmlFor="feminine-products">Feminine Products</label>
-          <select value ={selectedValue} onChange={handleDropdownChange} name="feminine-products">
+          <select value ={selectedValues.feminineProducts} onChange={e => handleDropdownChange(e, 'feminineProducts')} name="feminineProducts">
             <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
           </select>
           <label htmlFor="hand-sanitizer">Hand Sanitizer</label>
-          <select value ={selectedValue} onChange={handleDropdownChange} name="hand-sanitizer">
+          <select value ={selectedValues.handSanitizer} onChange= {e => handleDropdownChange(e, 'handSanitizer')} name="handSanitizer">
             <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
           </select>
           <label htmlFor="masks">Masks</label>
-          <select value ={selectedValue} onChange={handleDropdownChange} name="masks">
+          <select value ={selectedValues.masks} onChange= {e => handleDropdownChange(e, 'masks')} name="masks">
             <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
@@ -45,21 +64,21 @@ const handleDropdownChange = (e,index) => {
         <div>
           <h2 className="category-name food">Food</h2>
           <label htmlFor="canned-food">Canned Food</label>
-          <select value ={selectedValue} onChange={handleDropdownChange} name="canned-food">
+          <select value ={selectedValues.cannedFood} onChange= {e => handleDropdownChange(e, 'cannedFood')} name="cannedFood">
             <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
           </select>
           <label htmlFor="fruit">Fruit</label>
-          <select value ={selectedValue} onChange={handleDropdownChange} name="fruit">
+          <select value ={selectedValues.fruit} onChange= {e => handleDropdownChange(e, 'fruit')} name="fruit">
             <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
           </select>
           <label htmlFor="meat">Meat</label>
-          <select value ={selectedValue} onChange={handleDropdownChange} name="meat">
+          <select value ={selectedValues.meat} onChange= {e => handleDropdownChange(e, 'meat')} name="meat">
             <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
@@ -69,21 +88,21 @@ const handleDropdownChange = (e,index) => {
         <div>
           <h2 className="category-name clothing">Clothing</h2>
           <label htmlFor="socks">Socks</label>
-          <select value ={selectedValue} onChange={handleDropdownChange} name="socks">
+          <select value ={selectedValues.socks} onChange= {e => handleDropdownChange(e, 'socks')} name="socks">
             <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
           </select>
           <label htmlFor="underwear">Underwear</label>
-          <select value ={selectedValue} onChange={handleDropdownChange} name="underwear">
+          <select value ={selectedValues.underwear} onChange= {e => handleDropdownChange(e, 'underwear')} name="underwear">
             <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
           </select>
           <label htmlFor="hats">Hats</label>
-          <select value ={selectedValue} onChange={handleDropdownChange} name="hats">
+          <select value ={selectedValues.hats} onChange= {e => handleDropdownChange(e, 'hats')} name="hats">
             <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
