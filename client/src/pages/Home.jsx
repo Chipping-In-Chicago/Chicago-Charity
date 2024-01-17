018b0576c9c503e1ab925c1f5f4bbd28ea60c0a8
Flunder620 <jguzman12310@yahoo.com>
2024-01-16 20:19:45 -0600

    temporarily removed list items

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@apollo/client";
 import "./css/Home.scss";
 import { Link } from 'react-router-dom'
-import { QUERY_ITEMS, QUERY_PROFILES } from "../utils/queries";
+import { QUERY_ITEMS } from "../utils/queries";
 
 export default function Home() {
   const { loading, data } = useQuery(QUERY_ITEMS)
@@ -20,25 +20,25 @@ export default function Home() {
         <div>
           <h3> Current number of Medical Items donated:</h3>
           <ul className="item-list">
-            <li>Feminine Products: {item[0].itemCount}</li>
+            {/* <li>Feminine Products: {item[0].itemCount}</li>
             <li>Hand Sanitizer: {item[1].itemCount}</li>
-            <li>Masks: {item[2].itemCount}</li>
+            <li>Masks: {item[2].itemCount}</li> */}
           </ul>
         </div>
         <div>
           <h3> Current number of Food Items donated:</h3>
           <ul className="item-list">
-            <li>Canned Food: {item[3].itemCount}</li>
+            {/* <li>Canned Food: {item[3].itemCount}</li>
             <li>Fruit: {item[4].itemCount}</li>
-            <li>Meat: {item[5].itemCount}</li>
+            <li>Meat: {item[5].itemCount}</li> */}
           </ul>
         </div>
         <div>
           <h3> Current number of Clothing Items donated:</h3>
           <ul className="item-list">
-            <li>Socks: {item[6].itemCount}</li>
+            {/* <li>Socks: {item[6].itemCount}</li>
             <li>Underwear: {item[7].itemCount}</li>
-            <li>Hats: {item[8].itemCount}</li>
+            <li>Hats: {item[8].itemCount}</li> */}
           </ul>
         </div>
         <Link to = '/signup' className="btn">I need to help</Link>
